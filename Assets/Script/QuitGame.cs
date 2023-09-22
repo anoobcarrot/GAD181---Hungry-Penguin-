2722c678fc8ee1dd80517fcbe9fea7d130b3a388
anoobcarrot <136556154+anoobcarrot@users.noreply.github.com>
2023-09-22 16:53:16 +1000

    Initial + Minor things added
    - added main menu scene with buttons (some are WIP but start and quit button is set
    - added a start tutorial scene UI which shows video animations on how to walk and jump
    - added a level 1 where collecting blue fish can activate slide with 'S' for a limited duration, must collect fish to exit through the door
    - added a level 2 where collecting a red fish can activate fly with 'W' for a limited duration, must collect fish to exit through the door
    - all scenes fade in and out
    - added player movement script to control player movement
    - added animations to pixel player character
    - added an end scene where it shows a video then returns to main menu
    - added timer for each level to be 10 seconds, level restarts if timer runs out
    - pixel collider script so the collider detects pixel (must reference this when i find it again)
    - WHAT TO DO NEXT - add a red fish and blue fish tutorial UI

---
+++ Assets/Script/QuitGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{ 
+    public void Quit()
+    {
+        // Quit the application after a short delay
+        StartCoroutine(QuitWithDelay());
+    }
+
+    private IEnumerator QuitWithDelay()
+    {
+        // Wait for a short delay before quitting
+        yield return new WaitForSeconds(1f); // Adjust the delay as needed
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
